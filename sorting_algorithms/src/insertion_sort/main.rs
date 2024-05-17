140043cbdec9368c857827db890f8816c6b16b49
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-17 15:54:04 -0500

    Insertion Sort with binary search.

--- sorting_algorithms/src/insertion_sort/main.rs
+++ sorting_algorithms/src/insertion_sort/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let n = 6;
     let mut a = [5, 2, 4, 6, 1, 3];
-    InsertionSort::recursive_sort(&mut a, n);
+    InsertionSort::sort_with_binary_search(&mut a, n);
     print!("[");
     for elem in a {
         print!(" {} ", elem);
@@ -12,8 +12,8 @@ fn main() {
 struct InsertionSort {}
 
 impl InsertionSort {
-    fn sort(a: &mut [i32]) {
-        for i in 1..a.len() {
+    fn sort(a: &mut [i32], n: usize) {
+        for i in 1..n {
             let key = a[i];
             let mut j = i;
             while j > 0 && a[j - 1] > key {
@@ -34,5 +34,26 @@ impl InsertionSort {
                 a[i] = key;
             }
         }
+    }
+
+    fn sort_with_binary_search(a: &mut [i32], n: usize) {
+        for i in 1..n {
+            let key = a[i];
+            let index = Self::binary_insert(a, key, 0, i);
+            let mut j = i;
+            while j > index {
+                a[j] = a[j - 1];
+                j = j - 1;  // an usize can't be negative
+            }
+            a[j] = key;
+        }
+    }
+
+    fn binary_insert(a: &[i32], key: i32, p: usize, r: usize) -> usize {
+        let q = (p + r) / 2;
+        if p >= r { return p; }
+        if key == a[q] {return q;}
+        if key < a[q] {return Self::binary_insert(a, key, p, q)}
+        return Self::binary_insert(a, key, q + 1, r);
     }
 }
