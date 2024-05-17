977445f280f86bfb3d53f426257823b775b75197
sadelcarpio <sadelcarpioa@gmail.com>
2024-05-16 23:00:47 -0500

    Recursive Insertion Sort

--- sorting_algorithms/src/insertion_sort/main.rs
+++ sorting_algorithms/src/insertion_sort/main.rs
@@ -1,6 +1,7 @@
 fn main() {
+    let n = 6;
     let mut a = [5, 2, 4, 6, 1, 3];
-    InsertionSort::sort(&mut a);
+    InsertionSort::recursive_sort(&mut a, n);
     print!("[");
     for elem in a {
         print!(" {} ", elem);
@@ -20,6 +21,18 @@ impl InsertionSort {
                 j = j - 1;  // an usize can't be negative
             }
             a[j] = key;
+        }
+    }
+
+    fn recursive_sort(a: &mut [i32], n: usize) {
+        if n == 1 { return; }
+        Self::recursive_sort(a, n - 1);
+        let key: i32 = a[n - 1];
+        for i in (0..n - 1).rev() {
+            if key < a[i] {
+                a[i + 1] = a[i];
+                a[i] = key;
+            }
         }
     }
 }
